415cf5afce4f9689287dbc452ce5a80d4ce47d13
Prakhar Gupta <gprakhar2000@gmail.com>
2021-05-25 23:29:10 +0530

    updating

--- Hashmaps/Print Intersection.cpp
+++ Hashmaps/Print Intersection.cpp
@@ -1,4 +1,4 @@
-/*ou have been given two integer arrays/lists (ARR1 and ARR2) of size N and M, respectively. You need to print their intersection; An intersection for this problem can be defined when both the arrays/lists contain a particular value or to put it in other words, when there is a common value that exists in both the arrays/lists.
+/*You have been given two integer arrays/lists (ARR1 and ARR2) of size N and M, respectively. You need to print their intersection; An intersection for this problem can be defined when both the arrays/lists contain a particular value or to put it in other words, when there is a common value that exists in both the arrays/lists.
 Note :
 Input arrays/lists can contain duplicate elements.
 
---
+++ Tries and Huffman Coding/Auto complete.cpp
@@ -0,0 +1,124 @@
+/*Given n number of words and an incomplete word w. You need to auto-complete that word w.
+That means, find and print all the possible words which can be formed using the incomplete word w.
+Note : Order of words does not matter.
+Input Format :
+The first line of input contains an integer, that denotes the value of n.
+The following line contains n space separated words.
+The following line contains the word w, that has to be auto-completed.
+Output Format :
+Print all possible words in separate lines.
+Constraints:
+Time Limit: 1 sec
+Sample Input 1 :
+7
+do dont no not note notes den
+no
+Sample Output 2 :
+no
+not
+note
+notes
+Sample Input 2 :
+7
+do dont no not note notes den
+de
+Sample Output 2 :
+den
+Sample Input 3 :
+7
+do dont no not note notes den
+nom
+Sample Output 3 :
+(Empty) There is no word which starts with "nom"
+*/
+
+class TrieNode {
+   public:
+    char data;
+    TrieNode **children;
+    bool isTerminal;
+
+    TrieNode(char data) {
+        this->data = data;
+        children = new TrieNode *[26];
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+
+class Trie {
+    TrieNode *root;
+
+   public:
+    int count;
+
+    Trie() {
+        this->count = 0;
+        root = new TrieNode('\0');
+    }
+
+    bool insertWord(TrieNode *root, string word) {
+        // Base case
+        if (word.size() == 0) {
+            if (!root->isTerminal) {
+                root->isTerminal = true;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // Small calculation
+        int index = word[0] - 'a';
+        TrieNode *child;
+
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+        } else {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+
+        // Recursive call
+        return insertWord(child, word.substr(1));
+    }
+
+    void insertWord(string word) {
+        if (insertWord(root, word)) {
+            this->count++;
+        }
+    }
+	TrieNode* search(TrieNode *root, string word){
+        if(word.size()==0){
+            return root;
+        }
+        int index=word[0]-'a';
+        if(root->children[index]!=NULL){
+            return search(root->children[index],word.substr(1));
+        }
+        else return NULL;
+    }
+    void printAllPossible(TrieNode *root, string word){
+        if(root==NULL){
+            return;
+        }
+        if(root->isTerminal==true){
+            cout<<word<<endl;
+        }
+        for(int i=0;i<26;i++){
+            if(root->children[i]!=NULL){
+                printAllPossible(root->children[i],word+root->children[i]->data);
+            } 
+        }
+    }
+    void autoComplete(vector<string> input, string pattern) {
+        // Write your code here
+        for(int i=0;i<input.size();i++){
+            insertWord(input[i]);
+        }
+        TrieNode* temp=search(root,pattern);
+        printAllPossible(temp,pattern);
+    }
+};
